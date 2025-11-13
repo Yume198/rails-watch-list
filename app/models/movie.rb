3cf3c7e5c976abcc14b1de95d3682d4e6692d302
yume towhida <towhida.h.yume@gmail.com>
2025-11-13 14:15:46 +0900

    WIP commit for models migration routes controllers

---
+++ app/models/movie.rb
@@ -0,0 +1,9 @@
+class Movie < ApplicationRecord
+end
+
+class Movie < ApplicationRecord
+  has_many :bookmarks
+
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
+end
