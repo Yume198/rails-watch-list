d560cec32b50fac845459100a9709467d01cf0b4
yume towhida <towhida.h.yume@gmail.com>
2025-11-14 11:51:37 +0900

    WIP commit w cloudinary setup

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,8 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
 
+  has_one_attached :photo
+
   has_many :reviews, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
