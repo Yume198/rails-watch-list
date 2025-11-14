aa606195809322fe7bfca3e261e6a5d81075b4b2
yume towhida <towhida.h.yume@gmail.com>
2025-11-14 16:58:14 +0900

    fixed reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,7 +7,7 @@ class ReviewsController < ApplicationController
       redirect_to list_path(@list), notice: "Added New Review"
     else
       @bookmark = Bookmark.new
-      render :show, status: :unprocessable_entity
+      render :create, status: :unprocessable_entity
     end
   end
 
